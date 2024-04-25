97c1f467614aac4e3b8d751c7155bdf3a013cd6b
wilinton9 <159450242+wilinton9@users.noreply.github.com>
2024-04-24 22:29:19 -0500

    Update Principal.java

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,39 @@
 package com.mycompany.libropoo;
 
+    import java.util.Scanner;
+
 public class Principal {
-    
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingrese el título del libro:");
+        String titulo= sc.nextLine();
+        
+        System.out.println("Ingrese el autor del libro:");
+        String autor= sc.nextLine();
+        
+        System.out.println("Ingrese el año de publicación del libro:");
+        int annopublicacion= sc.nextInt();
+        
+        boolean leido=false;
+        
+        Libro libro = new Libro (titulo, autor,annopublicacion, leido);
+        
+        libro.mostrarInformacion();
+        System.out.println(libro.mostrarInformacion());
+        
+        libro.marcarLeido();
+        if (libro.esLeido()==true){
+            System.out.println("El libro "+ titulo+ " ya fue leido");
+        } else {
+            System.out.println("El libro "+ titulo+ " no ha sido leido");
+        }
+        
+        if (libro.esAntiguo()==true){
+            System.out.println("El libro "+ titulo+ " es antiguo");
+        }
+        
+        
+     }
+}
 }
